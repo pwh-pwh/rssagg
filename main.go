fff4c9ad80c178758b858012e637605100b429b2
pwh-pwh <plmoknpwh@163.com>
2023-05-12 00:01:57 +0800

    add godotenv

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello world")
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		log.Fatal("can not load env:port")
+	}
 }
